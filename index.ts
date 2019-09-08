711f92c32cc7220dd77516af0a6349ec40a0e0a1
Jack Mahoney <jackmahoney212@gmail.com>
2019-09-08 12:54:02 +0200

    tests and new methods

--- index.ts
+++ index.ts
@@ -3,20 +3,21 @@
  * @see https://github.com/mailslurp/swagger-sdk-typescript-fetch for more information
  */
 import {
-  BulkSendEmailOptions,
-  CommonOperationsApi,
-  CreateWebhookOptions,
-  Email,
-  EmailPreview,
-  ExtraOperationsApi,
-  Inbox,
-  SendEmailOptions,
-  Webhook
-} from "mailslurp-swagger-sdk-ts"
-import debug from "debug"
+    BulkSendEmailOptions,
+    CommonOperationsApi,
+    CreateWebhookOptions,
+    Email,
+    EmailPreview,
+    ExtraOperationsApi,
+    Inbox,
+    SendEmailOptions,
+    Webhook,
+} from 'mailslurp-swagger-sdk-ts';
+import debug from 'debug';
+import { MatchOptions } from 'mailslurp-swagger-sdk-ts/api';
 
 // setup logger. enable output with DEBUG=mailslurp-client env variable
-const log = debug("mailslurp-client");
+const log = debug('mailslurp-client');
 
 /**
  * MailSlurp config
@@ -26,9 +27,9 @@ const log = debug("mailslurp-client");
  * [Obtain your API Key](https://app.mailslurp.com) in your dashboard.
  */
 export type Config = {
-  // obtain an apiKey at https://app.mailslurp.com
-  apiKey?: string
-}
+    // obtain an apiKey at https://app.mailslurp.com
+    apiKey?: string;
+};
 
 /**
  * Options for advanced message fetching
@@ -37,28 +38,28 @@ export type Config = {
  * For more control over fetching. See also Webhook endpoints
  */
 export type GetMessagesOptions = {
-  // max emails to return
-  limit?: number,
-  // minimum number of emails to expect.
-  // when give, server will retry databases until this number is met or the retry timeout is exceeded
-  minCount?: number,
-  // maximum time to wait for conditions to be met
-  retryTimeout?: number,
-  // ignore emails received before this ISO-8601 date time
-  since?: Date
-}
+    // max emails to return
+    limit?: number;
+    // minimum number of emails to expect.
+    // when give, server will retry databases until this number is met or the retry timeout is exceeded
+    minCount?: number;
+    // maximum time to wait for conditions to be met
+    retryTimeout?: number;
+    // ignore emails received before this ISO-8601 date time
+    since?: Date;
+};
 
 // helper
 async function logCall<T>(tag: String, fn: () => Promise<T>): Promise<T> {
-  log("[%s] executing", tag);
-  try {
-    const result = await fn();
-    log("[%s] returned %O", tag, result);
-    return result;
-  } catch (e) {
-    log("[%s] threw exception %O", tag, e);
-    throw e;
-  }
+    log('[%s] executing', tag);
+    try {
+        const result = await fn();
+        log('[%s] returned %O', tag, result);
+        return result;
+    } catch (e) {
+        log('[%s] threw exception %O', tag, e);
+        throw e;
+    }
 }
 
 /**
@@ -71,192 +72,311 @@ async function logCall<T>(tag: String, fn: () => Promise<T>): Promise<T> {
  *  ```
  */
 export class MailSlurp {
+    /**
+     * @ignore
+     */
+    private commonOperationsApi: CommonOperationsApi;
+    /**
+     * @ignore
+     */
+    private extraOperationsApi: ExtraOperationsApi;
 
-  /**
-   * @ignore
-   */
-  private commonOperationsApi: CommonOperationsApi;
-  /**
-   * @ignore
-   */
-  private extraOperationsApi: ExtraOperationsApi;
-
-  constructor(opts: Config) {
-    // check options
-    if (!opts.apiKey) {
-      throw "Missing apiKey config parameter"
+    constructor(opts: Config) {
+        // check options
+        if (!opts.apiKey) {
+            throw 'Missing apiKey config parameter';
+        }
+        // instantiate api clients
+        const conf = { apiKey: opts.apiKey };
+        this.commonOperationsApi = new CommonOperationsApi(conf);
+        this.extraOperationsApi = new ExtraOperationsApi(conf);
     }
-    // instantiate api clients
-    const conf = {apiKey: opts.apiKey};
-    this.commonOperationsApi = new CommonOperationsApi(conf);
-    this.extraOperationsApi = new ExtraOperationsApi(conf)
-  }
-
-
-  /**
-   * Create a new email address and associated inbox
-   *
-   * @remarks
-   *
-   * All none-paid accounts use the `@mailslurp.com` domain.
-   * Custom domains available with plans.
-   */
-  async createNewEmailAddress(): Promise<Inbox> {
-    return logCall("createNewEmailAddress", () => this.commonOperationsApi.createNewEmailAddress())
-  }
-
-  /**
-   * Send an email from a random address
-   *
-   * @remarks
-   * To send from a known address first create an inbox and then use
-   * the sendEmail endpoints.
-   *
-   * @param sendEmailOptions
-   */
-  async sendEmailSimple(sendEmailOptions: SendEmailOptions): Promise<Response> {
-    return logCall("sendEmailSimple", () => this.commonOperationsApi.sendEmailSimple(sendEmailOptions))
-  }
-
-  /**
-   * Wait for an email to arrive at an inbox or return first found result
-   *
-   * @remarks
-   * Retries the call until at least one email is found or a maximum timeout is exceeded
-   *
-   *
-   * @param inboxEmailAddress
-   * @param inboxId
-   */
-  async waitForLatestEmail(inboxEmailAddress?: string, inboxId?: string): Promise<Email> {
-    if (!inboxId && !inboxEmailAddress) {
-      throw "Must provide either inboxId or emailAddress of inbox that you want to fetch from"
-    }
-    return logCall("waitForLatestEmail", () => this.commonOperationsApi.waitForLatestEmail(inboxEmailAddress, inboxId))
-  }
-
-  /**
-   * Wait for email at the given zero-based index in an inbox
-   * @param inboxId of the email address
-   * @param index zero based index
-   */
-  async waitForNthEmail(inboxId: string, index: number): Promise<Email> {
-    return logCall("waitForNthEmail", () => this.commonOperationsApi.waitForNthEmail(inboxId, index))
-  }
-
-  /**
-   * Create an inbox / email address
-   */
-  async createInbox(): Promise<Inbox> {
-    return logCall("createInbox", () => this.extraOperationsApi.createInbox())
-  }
-
-  /**
-   * Delete an inbox by id
-   * @param inboxId
-   */
-  async deleteInbox(inboxId: string): Promise<Response> {
-    return logCall("createInbox", () => this.extraOperationsApi.deleteInbox(inboxId));
-  }
-
-  /**
-   * Get an inbox by id
-   * @param inboxId
-   */
-  async getInbox(inboxId: string): Promise<Inbox> {
-    return logCall("getInbox", () => this.extraOperationsApi.getInbox(inboxId));
-  }
-
-  /**
-   * Get all inboxes
-   */
-  async getInboxes(): Promise<Inbox[]> {
-    return logCall("getInboxes", () => this.extraOperationsApi.getInboxes());
-  }
-
-  /**
-   * Get all emails in an inbox as EmailPreviews. To get the full email, use the getEmail endpoint
-   * @param inboxId
-   * @param args
-   */
-  async getEmails(inboxId: string, args: GetMessagesOptions = {}): Promise<EmailPreview[]> {
-    return logCall("getEmails", () => this.extraOperationsApi.getEmails(inboxId, args.limit, args.minCount, args.retryTimeout, args.since));
-  }
-
-  /**
-   * Get a full email from by id
-   * @param emailId
-   */
-  async getEmail(emailId: string): Promise<Email> {
-    return logCall("getEmail", () => this.extraOperationsApi.getEmail(emailId));
-  }
-
-  /**
-   * Get an email's raw contents from by id
-   * @param emailId
-   */
-  async getRawEmail(emailId: string): Promise<string> {
-    return logCall("getRawEmail", () => this.extraOperationsApi.getRawEmailContents(emailId));
-  }
-
-  /**
-   * Send and email from a given inbox
-   * @param inboxId
-   * @param sendEmailOptions
-   */
-  async sendEmail(inboxId: string, sendEmailOptions: SendEmailOptions): Promise<Response> {
-    return logCall("sendEmail", () => this.extraOperationsApi.sendEmail(inboxId, sendEmailOptions));
-  }
-
-  /**
-   * Bulk send emails
-   */
-  async bulkSendEmails(bulkSendEmailOptions: BulkSendEmailOptions): Promise<Response> {
-    return logCall("bulkSendEmails", () => this.extraOperationsApi.bulkSendEmails(bulkSendEmailOptions));
-  }
-
-  /**
-   * Bulk create inboxes
-   */
-  async bulkCreateInboxes(count: number): Promise<Inbox[]> {
-    return logCall("bulkCreateInboxes", () => this.extraOperationsApi.bulkCreateInboxes(count));
-  }
-
-  /**
-   * Bulk delete inboxes
-   */
-  async bulkDeleteInboxes(inboxIds: string[]): Promise<Response> {
-    return logCall("bulkDeleteInboxes", () => this.extraOperationsApi.bulkDeleteInboxes(inboxIds));
-  }
-
-  /**
-   * Create a webhook for notifications
-   */
-  async createWebhook(inboxId: string, createWebhookOptions: CreateWebhookOptions): Promise<Webhook> {
-    return logCall("createWebhook", () => this.extraOperationsApi.createWebhook(inboxId, createWebhookOptions))
-  }
 
     /**
-   * Get email attachment by id
-   */
-  async downloadAttachment(emailId: string, attachmentId: string): Promise<Response> {
-    return logCall("downloadAttachment", () => this.extraOperationsApi.downloadAttachment(attachmentId, emailId))
-  }
+     * Create a new email address / inbox
+     *
+     * @remarks
+     * Returns id and emailAddress of created inbox.
+     * All none-paid accounts use the `@mailslurp.com` domain.
+     * Custom domains available with plans.
+     */
+    async createNewEmailAddress(): Promise<Inbox> {
+        return logCall('createNewEmailAddress', () =>
+            this.commonOperationsApi.createNewEmailAddress()
+        );
+    }
 
-  /**
-   * @deprecated
-   */
-  async getEmailAttachment(emailId: string, attachmentId: string): Promise<Response> {
-    return this.downloadAttachment(emailId, attachmentId)
-  }
+    /**
+     * Send an email from a random address
+     *
+     * @remarks
+     * To send from a known address first create an inbox and then use
+     * the sendEmail endpoints.
+     *
+     * @param sendEmailOptions
+     */
+    async sendEmailSimple(
+        sendEmailOptions: SendEmailOptions
+    ): Promise<Response> {
+        return logCall('sendEmailSimple', () =>
+            this.commonOperationsApi.sendEmailSimple(sendEmailOptions)
+        );
+    }
 
-  /**
-   * @deprecated
-   */
-  async fetchLatestEmail(inboxEmailAddress?: string, inboxId?: string): Promise<Email> {
-    return this.waitForLatestEmail(inboxEmailAddress, inboxId)
-  }
+    /**
+     * Wait for an email to arrive at an inbox or return first found result
+     *
+     * @remarks
+     * Retries the call until at least one email is found or a maximum timeout is exceeded
+     *
+     * @param inboxId
+     * @param timeout max milliseconds to wait
+     */
+    async waitForLatestEmail(
+        inboxId?: string,
+        timeout?: number
+    ): Promise<Email> {
+        return logCall('waitForLatestEmail', () =>
+            this.commonOperationsApi.waitForLatestEmail(inboxId, timeout)
+        );
+    }
 
+    async waitForNthEmail(
+        inboxId: string,
+        index: number,
+        timeout?: number
+    ): Promise<Email> {
+        return logCall('waitForNthEmail', () =>
+            this.commonOperationsApi.waitForNthEmail(inboxId, index, timeout)
+        );
+    }
+
+    /**
+     * Wait until both count and match options are met and return list of emails.
+     *
+     * @remarks
+     * Match options allow simple CONTAINS or EQUALS filtering on SUBJECT, TO, BCC, CC, and FROM.
+     *
+     * @param matchOptions
+     * @param count
+     * @param inboxId
+     * @param timeout  timeout max milliseconds to wait
+     */
+    async waitForMatchingEmails(
+        matchOptions: MatchOptions,
+        count?: number,
+        inboxId?: string,
+        timeout?: number
+    ) {
+        return logCall('waitForMatchingEmail', () =>
+            this.commonOperationsApi.waitForMatchingEmail(
+                matchOptions,
+                count,
+                inboxId,
+                timeout
+            )
+        );
+    }
+
+    /**
+     * Wait for and return list of emails with length of given count
+     * @param count
+     * @param inboxId
+     * @param timeout
+     */
+    async waitForEmailCount(
+        count?: number,
+        inboxId?: string,
+        timeout?: number
+    ) {
+        return logCall('waitForEmailCount', () =>
+            this.commonOperationsApi.waitForEmailCount(count, inboxId, timeout)
+        );
+    }
+
+    /**
+     * Delete all emails in a given inbox
+     * @param inboxId
+     */
+    async emptyInbox(inboxId: string): Promise<Response> {
+        return logCall('emptyInbox', () =>
+            this.commonOperationsApi.emptyInbox(inboxId)
+        );
+    }
+
+    /**
+     * Delete an email by id
+     * @param emailId
+     */
+    async deleteEmail(emailId: string): Promise<Response> {
+        return log('deleteEmail', () =>
+            this.commonOperationsApi.deleteEmail(emailId)
+        );
+    }
+
+    /**
+     * Delete an email by id
+     * @param emailId
+     */
+    async deleteEmailAddress(emailId: string): Promise<Response> {
+        return log('deleteEmailAddress', () =>
+            this.commonOperationsApi.deleteEmailAddress(emailId)
+        );
+    }
+
+    /**
+     * Create an inbox / email address
+     */
+    async createInbox(): Promise<Inbox> {
+        return logCall('createInbox', () =>
+            this.extraOperationsApi.createInbox()
+        );
+    }
+
+    /**
+     * Delete an inbox by id
+     * @param inboxId
+     */
+    async deleteInbox(inboxId: string): Promise<Response> {
+        return logCall('createInbox', () =>
+            this.extraOperationsApi.deleteInbox(inboxId)
+        );
+    }
+
+    /**
+     * Get an inbox by id
+     * @param inboxId
+     */
+    async getInbox(inboxId: string): Promise<Inbox> {
+        return logCall('getInbox', () =>
+            this.extraOperationsApi.getInbox(inboxId)
+        );
+    }
+
+    /**
+     * Get all inboxes
+     */
+    async getInboxes(): Promise<Inbox[]> {
+        return logCall('getInboxes', () =>
+            this.extraOperationsApi.getInboxes()
+        );
+    }
+
+    /**
+     * Get all emails in an inbox as EmailPreviews. To get the full email, use the getEmail endpoint
+     * @param inboxId
+     * @param args
+     */
+    async getEmails(
+        inboxId: string,
+        args: GetMessagesOptions = {}
+    ): Promise<EmailPreview[]> {
+        return logCall('getEmails', () =>
+            this.extraOperationsApi.getEmails(
+                inboxId,
+                args.limit,
+                args.minCount,
+                args.retryTimeout,
+                args.since
+            )
+        );
+    }
+
+    /**
+     * Get a full email from by id
+     * @param emailId
+     */
+    async getEmail(emailId: string): Promise<Email> {
+        return logCall('getEmail', () =>
+            this.extraOperationsApi.getEmail(emailId)
+        );
+    }
+
+    /**
+     * Get an email's raw contents from by id
+     * @param emailId
+     */
+    async getRawEmail(emailId: string): Promise<string> {
+        return logCall('getRawEmail', () =>
+            this.extraOperationsApi.getRawEmailContents(emailId)
+        );
+    }
+
+    /**
+     * Send and email from a given inbox
+     * @param inboxId
+     * @param sendEmailOptions
+     */
+    async sendEmail(
+        inboxId: string,
+        sendEmailOptions: SendEmailOptions
+    ): Promise<Response> {
+        return logCall('sendEmail', () =>
+            this.extraOperationsApi.sendEmail(inboxId, sendEmailOptions)
+        );
+    }
+
+    /**
+     * Bulk send emails
+     */
+    async bulkSendEmails(
+        bulkSendEmailOptions: BulkSendEmailOptions
+    ): Promise<Response> {
+        return logCall('bulkSendEmails', () =>
+            this.extraOperationsApi.bulkSendEmails(bulkSendEmailOptions)
+        );
+    }
+
+    /**
+     * Bulk create inboxes
+     */
+    async bulkCreateInboxes(count: number): Promise<Inbox[]> {
+        return logCall('bulkCreateInboxes', () =>
+            this.extraOperationsApi.bulkCreateInboxes(count)
+        );
+    }
+
+    /**
+     * Bulk delete inboxes
+     */
+    async bulkDeleteInboxes(inboxIds: string[]): Promise<Response> {
+        return logCall('bulkDeleteInboxes', () =>
+            this.extraOperationsApi.bulkDeleteInboxes(inboxIds)
+        );
+    }
+
+    /**
+     * Create a webhook for notifications
+     */
+    async createWebhook(
+        inboxId: string,
+        createWebhookOptions: CreateWebhookOptions
+    ): Promise<Webhook> {
+        return logCall('createWebhook', () =>
+            this.extraOperationsApi.createWebhook(inboxId, createWebhookOptions)
+        );
+    }
+
+    /**
+     * Create a webhook for notifications
+     */
+    async deleteWebhook(inboxId: string, webhookId: string): Promise<Response> {
+        return logCall('deleteWebhook', () =>
+            this.extraOperationsApi.deleteWebhook(inboxId, webhookId)
+        );
+    }
+
+    /**
+     * Get email attachment by id
+     */
+    async downloadAttachment(
+        emailId: string,
+        attachmentId: string
+    ): Promise<Response> {
+        return logCall('downloadAttachment', () =>
+            this.extraOperationsApi.downloadAttachment(attachmentId, emailId)
+        );
+    }
 }
 
 export default MailSlurp;
--- integration.spec.ts
+++ integration.spec.ts
@@ -1,16 +1,86 @@
-declare global {
-  const test: any;
-  const expect: any;
-}
-import { MailSlurp } from "./index"
-import Default from "./index"
+import Default, { MailSlurp } from './index';
 
-test("that import was successful", () => {
-  expect(MailSlurp).not.toBeNull();
-  expect(Default).not.toBeNull();
+const createNewEmailAddress = jest.fn();
+const sendEmailSimple = jest.fn();
+const waitForLatestEmail = jest.fn();
+const waitForNthEmail = jest.fn();
+const waitForMatchingEmail = jest.fn();
+const waitForEmailCount = jest.fn();
+
+jest.mock('mailslurp-swagger-sdk-ts', () => {
+    return {
+        CommonOperationsApi: function() {
+            return {
+                createNewEmailAddress,
+                sendEmailSimple,
+                waitForLatestEmail,
+                waitForNthEmail,
+                waitForMatchingEmail,
+                waitForEmailCount,
+            };
+        },
+        ExtraOperationsApi: function() {
+            return {};
+        },
+    };
 });
-test("client can be instantiated", () => {
-  const client = new MailSlurp({ apiKey: 'test' })
-  expect(client).not.toBeNull();
-  expect(client.fetchLatestEmail).not.toBeNull();
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
+describe('importing client', () => {
+    test('that import was successful', () => {
+        expect(MailSlurp).not.toBeNull();
+        expect(Default).not.toBeNull();
+    });
+    test('client can be instantiated', () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        expect(client).not.toBeNull();
+        expect(client.createNewEmailAddress).not.toBeNull();
+    });
+});
+
+describe('functions are mapped correctly to common operations api', () => {
+    test('can create a new email address', async () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        await client.createNewEmailAddress();
+        expect(createNewEmailAddress).toHaveBeenCalledTimes(1);
+    });
+    test('can send email', async () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        const options = {
+            to: [''],
+        };
+        await client.sendEmailSimple(options);
+        expect(sendEmailSimple).toHaveBeenCalledWith(options);
+    });
+    test('can wait for latest email', async () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        const inboxId = 'inboxId';
+        const timeout = 123;
+        await client.waitForLatestEmail(inboxId, timeout);
+        expect(waitForLatestEmail).toHaveBeenCalledWith(inboxId, timeout);
+    });
+    test('can wait for nth email', async () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        const inboxId = 'inboxId';
+        const index = 2;
+        await client.waitForNthEmail(inboxId, index);
+        expect(waitForNthEmail).toHaveBeenCalledWith(inboxId, index, undefined);
+    });
+    test('can wait for matching email', async () => {
+        const client = new MailSlurp({ apiKey: 'test' });
+        const options = {};
+        const count = 2;
+        const inboxId = 'inboxId';
+        const timeout = 123;
+        await client.waitForMatchingEmails(options, count, inboxId, timeout);
+        expect(waitForMatchingEmail).toHaveBeenCalledWith(
+            options,
+            count,
+            inboxId,
+            timeout
+        );
+    });
 });
